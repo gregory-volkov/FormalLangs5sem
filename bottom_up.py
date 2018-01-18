6590546825d0d52aba875b89bfe73f318c3fc576
Volkov Grigoriy <grihanaft0510@gmail.com>
2018-01-19 01:13:50 +0300

    oops

--- bottom_up.py
+++ bottom_up.py
@@ -86,8 +86,7 @@ def bottom_up(rfa, graph):
 
 
 if len(sys.argv) > 1:
-    #res = bottom_up(get_rfa(sys.argv[1]), get_graph(sys.argv[2]))
-    res = bottom_up(gram2automata(get_grammar(sys.argv[1])), get_graph(sys.argv[2]))
+    res = bottom_up(get_rfa(sys.argv[1]), get_graph(sys.argv[2]))
     res_str = '\n'.join([','.join([str(i), nonterm, str(j)]) for i, nonterm, j in res])
     if len(sys.argv) > 3:
         with open(sys.argv[3], 'w') as f:
